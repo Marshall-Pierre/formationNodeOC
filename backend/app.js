81e7027b310af0344a10d7f926315846965019f5
NGUEMA ZENG Marshall-Pierre <nguemazengmp@gmail.com>
2022-11-08 17:37:56 +0100

    changement de pc

---
+++ backend/app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const Thing = require('./models/Thing');
+
+mongoose.connect('mongodb+srv://mp00:WdL0RI3yRE4lZevr@cluster0.ilarkcg.mongodb.net/?retryWrites=true&w=majority',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connection à MongoDb réussie !'))
+.catch(() => console.log('Connection à MongoDb échouée !'));
+
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body.id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregsitré !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+          {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+
+    res.status(200).json(stuff);
+});
+
+module.exports = app;
